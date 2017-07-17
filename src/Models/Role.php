b245952f1e354c68ec45eb498c4c15cbdc741217
folojona@gmail.com <topgasy@tsikawa.com>
2017-07-17 21:32:53 +0200

    kipa - updatingin

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -5,5 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model {
 
 	protected $table = "ry_admin_roles";
+	
+	public function user() {
+		return $this->belongsTo("App\User", "user_id");
+	}
 
 }
