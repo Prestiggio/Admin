04764a8204e26f1c1c863a3ba8a9ab859be4cf1e
Landr Rakotoarivelo <folojona@gmail.com>
2019-09-30 14:48:37 +0200

    login as another

---
+++ src/database/migrations/2019_03_22_125947_create_pretentions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePretentionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ry_admin_pretentions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('pretended_id');
+            $table->string('token');
+            $table->timestamp('expire_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ry_admin_pretentions');
+    }
+}
