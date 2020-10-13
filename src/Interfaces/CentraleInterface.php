09efc673dee5c2ca7fcdbf28395a444aead312db
Landr Rakotoarivelo <folojona@gmail.com>
2020-10-13 14:11:40 +0200

    marketplace fix

--- src/Interfaces/CentraleInterface.php
+++ src/Interfaces/CentraleInterface.php
@@ -8,5 +8,9 @@ interface CentraleInterface
     public function create($scoped_type, $scoped_id);
     
     public function activateUser(Request $request);
+    
+    public function buildBuyerUrl($absolute_url);
+    
+    public function buildSellerUrl($absolute_url);
 }
 ?>
