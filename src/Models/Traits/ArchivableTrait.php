729ec1b11314f719809c969b1a14eb1bac17a67c
Landr Rakotoarivelo <folojona@gmail.com>
2021-04-16 13:51:11 +0200

    archiver

--- src/Models/Traits/ArchivableTrait.php
+++ src/Models/Traits/ArchivableTrait.php
@@ -1,113 +1,59 @@
 <?php 
 namespace Ry\Admin\Models\Traits;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Ry\Admin\Models\Archive;
 
 trait ArchivableTrait
 {
-    private $archived = false;
+    private $archiver;
     
-    public static $CACHE = [];
+    public static $ARCHIVER_CLASS;
     
-    public function archive($frozen, $callback, $else_callback=null, $pending_callback=null) {
-        $archive = Archive::whereArchivableType(get_class($this))->whereArchivableId($this->id)->first();
-        if($frozen) {
-            if($archive) {
-                $this->archived = $archive->nsetup;
-                $this->archived['last_updated_at'] = $archive->update_at;
-            }
-            else {
-                call_user_func_array($callback, [$this]);
-                $result = $this->toArray();
+    public function archive() {
+        if(static::$ARCHIVER_CLASS) {
+            $archive = Archive::whereArchivableType(get_class($this))->whereArchivableId($this->id)->first();
+            if(!$archive) {
                 $archive = new Archive();
                 $archive->archivable_type = get_class($this);
                 $archive->archivable_id = $this->id;
+                $archive->nsetup = [
+                    'pending' => true
+                ];
+            }
+            $archiver = $this->getArchiver();
+            if($archiver->isClosed() && isset($archive->nsetup['closed'])) {
+                //do nothing, archive has no need to be updated so far
+            }
+            else {
+                $setup = $archive->nsetup;
+                $setup['pending'] = true;
+                $archive->nsetup = $setup;
+                $archive->save();
+                
+                $result = $archiver->toArray();
+                $result['pending'] = false;
+                if($archiver->isClosed())
+                    $result['closed'] = Carbon::now()->format('Y-m-d H:i:s');
                 $archive->nsetup = $result;
                 $archive->save();
-                $this->archived = $result;
-                $this->archived['last_updated_at'] = $archive->update_at;
-            }
-        }
-        else {
-            $request = Request::capture();
-            if($archive) {
-                if($request->userAgent()) {
-                    $this->archived = $archive->nsetup;
-                    $this->archived['last_updated_at'] = $archive->update_at;
-                }
-                else {
-                    $result = $archive->nsetup;
-                    $result['pending'] = true;
-                    $archive->nsetup = $result;
-                    $archive->save();
-                    
-                    call_user_func_array($callback, [$this]);
-                    $result = $this->toArray();
-                    $result['pending'] = false;
-                    $archive->nsetup = $result;
-                    $archive->save();
-                    $this->archived = $result;
-                    $this->archived['last_updated_at'] = $archive->update_at;
-                }
-            }
-            elseif($pending_callback) {
-                if($request->userAgent()) {
-                    $this->archived = call_user_func_array($pending_callback, [$this]);
-                    $result['pending'] = true;
-                    $this->archived['last_updated_at'] = null;
-                }
-                else {
-                    call_user_func_array($callback, [$this]);
-                    $result = $this->toArray();
-                    $result['pending'] = false;
-                    $archive = new Archive();
-                    $archive->archivable_type = get_class($this);
-                    $archive->archivable_id = $this->id;
-                    $archive->nsetup = $result;
-                    $archive->save();
-                    $this->archived = $result;
-                    $this->archived['last_updated_at'] = $archive->update_at;
-                }
-            }
-            elseif($else_callback) {
-                $is_archived = call_user_func_array($else_callback, [$this, true]);
-                if(!$is_archived) {
-                    call_user_func_array($callback, [$this]);
-                    $this->archived = call_user_func_array($else_callback, [$this]);
-                }
-                else {
-                    $this->archived = call_user_func_array($else_callback, [$this]);
-                }
             }
         }
     }
     
-    protected function isArchived() {
-        if(!$this->archived) {
-            if(!isset(static::$CACHE[get_class($this)][$this->id]))
-                static::$CACHE[get_class($this)][$this->id] = Archive::whereArchivableType(get_class($this))->whereArchivableId($this->id)->first();
-            $archive = static::$CACHE[get_class($this)][$this->id];
-            if($archive) {
-                $this->archived = $archive->nsetup;
-                $this->archived['archived'] = true;
+    public function getArchiver() {
+        if(static::$ARCHIVER_CLASS) {
+            if(!$this->archiver) {
+                $this->archiver = new static::$ARCHIVER_CLASS($this);
             }
-            else {
-                static::$CACHE[get_class($this)][$this->id] = false;
-                $this->archived = [
-                    'archived' => false
-                ];
-            }
+            return $this->archiver;
         }
-        return $this->archived;
+        return $this;
     }
     
     public function toArray() {
-        $ar = parent::toArray();
-        if($this->isArchived()) {
-            return array_replace_recursive($ar, $this->archived);
-        }
-        return $ar;
+        return $this->getArchiver()->toArray();
     }
     
     public function unarchive() {
